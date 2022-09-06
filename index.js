c4e2a4edf2e00eefd4e9b855401b1879c636a748
SAHILKHAN7001 <106735502+SAHILKHAN7001@users.noreply.github.com>
2022-09-06 12:37:37 +0530

    BoredBot

---
+++ index.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById("get-activity");
+
+btn.addEventListener("click",function() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("activity").textContent = ` ${data.activity}`
+        document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        document.body.classList.add("fun")
+    })
+})
